d37878596cbf5eb4834b0b6ad48eab28cd5979a5
ackermanGitHub <julio.sergio2709@gmail.com>
2023-05-21 15:25:43 -0400

    ➕: dotenv and pg

---
+++ src/server/db.ts
@@ -0,0 +1,41 @@
+import { Pool } from 'pg';
+
+const getPostgresUser = () => {
+  if (!process.env.POSTGRES_USER) {
+    throw new Error('POSTGRES_USER is not defined');
+  }
+  return process.env.POSTGRES_USER;
+};
+const getPostgresHost = () => {
+  if (!process.env.POSTGRES_HOST) {
+    throw new Error('POSTGRES_HOST is not defined');
+  }
+  return process.env.POSTGRES_HOST;
+};
+const getPostgresDatabase = () => {
+  if (!process.env.POSTGRES_DATABASE) {
+    throw new Error('POSTGRES_DATABASE is not defined');
+  }
+  return process.env.POSTGRES_DATABASE;
+};
+const getPostgresPassword = () => {
+  if (!process.env.POSTGRES_PASSWORD) {
+    throw new Error('POSTGRES_PASSWORD is not defined');
+  }
+  return process.env.POSTGRES_PASSWORD;
+};
+const getPostgresPort = () => {
+  if (!process.env.POSTGRES_PORT) {
+    throw new Error('POSTGRES_PORT is not defined');
+  }
+  return parseInt(process.env.POSTGRES_PORT);
+};
+
+export const pool = new Pool({
+  user: getPostgresUser(),
+  host: getPostgresHost(),
+  database: getPostgresDatabase(),
+  password: getPostgresPassword(),
+  port: getPostgresPort(),
+  ssl: true,
+});
